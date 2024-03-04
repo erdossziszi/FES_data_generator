e7629a99d66e74fff378b6cf3b5dc25da3826ee1
erdossziszi <erdos.sziszi@gmail.com>
2024-03-04 18:01:32 +0100

    Init commit for data generator

---
+++ FES_data_generator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FES_data_generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
