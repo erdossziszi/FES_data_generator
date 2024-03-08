10f3dc337a9b02b9e26faff0b9588dd4a5713988
erdossziszi <erdos.sziszi@gmail.com>
2024-03-08 15:41:10 +0100

    Add ExamAllData to Model

---
+++ FES_data_generator/Model/ExamAllData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FES_data_generator.Model
+{
+    internal class ExamAllData
+    {
+        public Exam? Exam { get; set; }
+        public AllConstraints? AllConstraints { get; set; }
+    }
+}
