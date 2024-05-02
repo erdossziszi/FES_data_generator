8634092f0b9f2235f77392d9d06dc48e060c04cd
erdossziszi <erdos.sziszi@gmail.com>
2024-05-02 15:02:09 +0200

    Generalisation for data generation

--- FES_data_generator/Program.cs
+++ FES_data_generator/Program.cs
@@ -12,87 +12,109 @@ namespace FES_data_generator
         {
 
             Random r = new Random();
-
-            for (int j = 0; j < 1; j++)
+            TimeSlots[] timeSlots =
             {
-                Exam testExam = new Exam()
-                {
-                    StudentsNr = 15,//r.Next(5, 301),
-                    InstructorsNr = 10,//r.Next(5, 101),
-                    DaysNr = 2,//r.Next(1, 16), //TODO: Feasibility check
-                    SlotsPerDay = 15,//r.Next(10, 145),
-                    AvailabilitySlotsLenght = 3, //must be dividable with SlotsPerDay
-                    RoomNr = 2,//r.Next(1, 6),
-                    ProgrammNr = 1,//r.Next(1, 5),
-                    DegreeNr = 2,//r.Next(1, 4),
-                    RolesNr = 0,//r.Next(5),  //nullable
-                    CoursesNr = 4,//r.Next(30) //nullable
-                };
-                Dictionary<int, HashSet<int>> instructorsOfProgramms = new Dictionary<int, HashSet<int>>();
-                for (int i = 1; i < testExam.ProgrammNr + 1; i++)
-                {
-                    instructorsOfProgramms.Add(i, new HashSet<int>());
-                }
+                new TimeSlots(10, [1,2,5,10], 1, 2),
+                new TimeSlots(15, [1,3,5,15], 1, 3),
+                new TimeSlots(20, [2,5,10,20], 2, 4),
+                new TimeSlots(40, [4,8,20,40], 4, 8),
+                new TimeSlots(60, [6,12,30,60], 6, 10)
+            };
 
-                GenerateInstructors(r, testExam, instructorsOfProgramms);
-                GetInstructorRolesPerProgram(testExam);
-                GenerateCourses(r, testExam);
-                GenerateStudents(r, testExam, instructorsOfProgramms);
+            for (int j = 0; j < 50; j++)
+            {
+                Run(r, j);
+            }
+        }
 
-                int[] minInstructorsPerRoles = new int[testExam.RolesNr];
-                for (int k = 0; k < testExam.RolesNr; k++)
+        private static void Run(Random r, int j)
+        {
+            Exam testExam = new Exam()
+            {
+                StudentsNr = 10,//r.Next(5, 301),    /////////////////////////////////////////////////////
+                InstructorsNr = r.Next(3, 101),
+                DaysNr = r.Next(1, 3), //TODO: Feasibility check
+                SlotsPerDay = 60,//r.Next(10, 145),  /////////////////////////////////////////////////////
+                AvailabilitySlotsLenght = 60, //must be dividable with SlotsPerDay  ///////////////////////
+                RoomNr = r.Next(1, 3),
+                ProgrammNr = r.Next(1, 3),
+                DegreeNr = r.Next(1, 4),
+                RolesNr = r.Next(4),  //nullable
+                CoursesNr = r.Next(11) //nullable
+            };
+            Dictionary<int, HashSet<int>> instructorsOfProgramms = new Dictionary<int, HashSet<int>>();
+            for (int i = 1; i < testExam.ProgrammNr + 1; i++)
+            {
+                instructorsOfProgramms.Add(i, new HashSet<int>());
+            }
+
+            GenerateInstructors(r, testExam, instructorsOfProgramms);
+            GetInstructorRolesPerProgram(testExam);
+            GenerateCourses(r, testExam);
+            GenerateStudents(r, testExam, instructorsOfProgramms);
+            AllConstraints testConstraints = GetAllConstraints(r, testExam);
+
+            ExamAllData testExamAllData = new ExamAllData()
+            {
+                Exam = testExam,
+                AllConstraints = testConstraints
+            };
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+            };
+            string fileName = $"S_{testExam.StudentsNr}_Ts_{testExam.SlotsPerDay}_A_{testExam.AvailabilitySlotsLenght}_Nr_{j}";
+            string fileNameJson = @"..\..\..\JSON files\" + fileName + ".json";
+            string fileNameDzn = @"..\..\..\DZN files\" + fileName + ".dzn";
+
+            string json = JsonSerializer.Serialize(testExamAllData, options);
+            //Console.WriteLine(json);
+            File.WriteAllText(fileNameJson, json);
+
+            var examToDzn = new ExamToDzn(testExam, testConstraints);
+            examToDzn.SerializeToDzn(fileNameDzn);
+        }
+
+        private static AllConstraints GetAllConstraints(Random r, Exam testExam)
+        {
+            int[] minInstructorsPerRoles = new int[testExam.RolesNr];
+            for (int k = 0; k < testExam.RolesNr; k++)
+            {
+                minInstructorsPerRoles[k] = testExam.InstructorsNr;
+
+                for (int i = 0; i < testExam.ProgrammNr; i++)
                 {
-                    minInstructorsPerRoles[k] = testExam.InstructorsNr;
-
-                    for (int i = 0; i < testExam.ProgrammNr; i++)
+                    if (testExam.InstructorRolesPerProgramm[i, k] != null)
                     {
-                        if (testExam.InstructorRolesPerProgramm[i, k] != null)
-                        {
-                            minInstructorsPerRoles[k] = Math.Min(minInstructorsPerRoles[k], testExam.InstructorRolesPerProgramm[i, k].Count);
-                        }
+                        minInstructorsPerRoles[k] = Math.Min(minInstructorsPerRoles[k], testExam.InstructorRolesPerProgramm[i, k].Count);
                     }
                 }
-                Dictionary<int, int> degreeExamLenPairs = Enumerable.Range(1, testExam.DegreeNr).ToDictionary(degree => degree, degree => new Random().Next(1, 3));
-                AllConstraints testConstraints = new AllConstraints()
-                {
-                    RolesDemands = new Constraint(true, Enumerable.Range(0, testExam.RolesNr).Select(x => r.Next(minInstructorsPerRoles[x] + 1)).ToArray()),
-                    RolesContinuity = new Constraint(true, Enumerable.Range(1, testExam.RolesNr).OrderBy(x => r.Next()).Take(r.Next(testExam.RolesNr)).ToArray()),
-                    ExamLen = new Constraint(true, testExam.Students.Select(student => degreeExamLenPairs[student.Degree]).ToArray()),
-                    LunchTsMinLen = new Constraint(true, r.Next(3)),
-                    LunchStarts = new Constraint(true, Enumerable.Range(1, testExam.DaysNr)
-                                      .ToDictionary(day => day, day => new int[] { testExam.SlotsPerDay/2 + (day - 1) * testExam.SlotsPerDay, testExam.SlotsPerDay / 2+1 + (day - 1) * testExam.SlotsPerDay })),
-                    SupervisorAvailable = new Constraint(false, 5),
-                    OptimalStartTs = new Constraint(false, [1, 2]),
-                    OptimalFinishTs = new Constraint(false, [1, testExam.SlotsPerDay - 1]),
-                    MinimizeRooms = new Constraint(false, r.Next(3, 6) * 100),
-                    RolesSoftContinuity = new Constraint(false, Enumerable.Range(1, testExam.RolesNr).ToDictionary(role => role, role => r.Next(3))),
-                    SameDegreeInRoom = new Constraint(false, r.Next(3) * 10),
-                    Mergeability = new Constraint(false, r.Next(3))
-                };
-                testConstraints.OptimalLunchLenght = new Constraint(false, [2, r.Next((int)testConstraints.LunchTsMinLen.SingleParam,3)]);
-
-                ExamAllData testExamAllData = new ExamAllData()
-                {
-                    Exam = testExam,
-                    AllConstraints = testConstraints
-                };
-
-                var options = new JsonSerializerOptions
-                {
-                    WriteIndented = true,
-                    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-                };
-                
-                string fileNameJson = @"..\..\..\JSON files\" + testExam.StudentsNr + "_" + testExam.InstructorsNr + "_" + j + DateTime.Now.ToString("yyyyMMdd_HHmm") + ".json";
-                string fileNameDzn = @"..\..\..\DZN files\" + testExam.StudentsNr + "_" + testExam.InstructorsNr + "_" + j + DateTime.Now.ToString("yyyyMMdd_HHmm") + ".dzn";
-
-                string json = JsonSerializer.Serialize(testExamAllData, options);
-                Console.WriteLine(json);
-                File.WriteAllText(fileNameJson, json);
-
-                var examToDzn = new ExamToDzn(testExam,testConstraints);
-                examToDzn.SerializeToDzn(fileNameDzn);
             }
+            Dictionary<int, int> degreeExamLenPairs = Enumerable.Range(1, testExam.DegreeNr).ToDictionary(degree => degree, degree => new Random().Next(1, 3)); ///////////
+            int minExamLen = degreeExamLenPairs.Min(x => x.Value);
+            AllConstraints testConstraints = new AllConstraints()
+            {
+                RolesDemands = new Constraint(true, Enumerable.Range(0, testExam.RolesNr).Select(x => r.Next(1, minInstructorsPerRoles[x] + 1)).ToArray()),
+                RolesContinuity = new Constraint(true, Enumerable.Range(1, testExam.RolesNr).OrderBy(x => r.Next()).Take(r.Next(testExam.RolesNr)).ToArray()),
+                ExamLen = new Constraint(true, testExam.Students.Select(student => degreeExamLenPairs[student.Degree]).ToArray()),
+                LunchTsMinLen = new Constraint(true, r.Next(minExamLen + 1)),
+                LunchStarts = new Constraint(true, Enumerable.Range(1, testExam.DaysNr)
+                                  .ToDictionary(day => day, day => new int[] {
+                                          r.Next(testExam.SlotsPerDay / 4 + (day - 1) * testExam.SlotsPerDay, testExam.SlotsPerDay/2 + (day - 1) * testExam.SlotsPerDay + 1),
+                                          r.Next(testExam.SlotsPerDay / 2 + 1 + (day - 1) * testExam.SlotsPerDay, testExam.SlotsPerDay * 3/4 + (day - 1) * testExam.SlotsPerDay + 1)
+                                  })),
+                SupervisorAvailable = new Constraint(false, r.Next(2) * 5),
+                OptimalStartTs = new Constraint(false, [r.Next(3), r.Next(minExamLen)]),
+                OptimalFinishTs = new Constraint(false, [r.Next(3), testExam.SlotsPerDay - r.Next(minExamLen)]),
+                MinimizeRooms = new Constraint(false, r.Next(6) * 100),
+                RolesSoftContinuity = new Constraint(false, Enumerable.Range(1, testExam.RolesNr).ToDictionary(role => role, role => r.Next(5))),
+                SameDegreeInRoom = new Constraint(false, r.Next(3) * 10),
+                Mergeability = new Constraint(false, r.Next(3))
+            };
+            testConstraints.OptimalLunchLenght = new Constraint(false, [r.Next(4), r.Next((int)testConstraints.LunchTsMinLen.SingleParam, minExamLen * 2)]);
+            return testConstraints;
         }
 
         private static void GenerateInstructors(Random r, Exam testExam, Dictionary<int, HashSet<int>> instructorsOfProgramms)
@@ -128,7 +150,7 @@ namespace FES_data_generator
                     Programm = r.Next(1, testExam.ProgrammNr + 1),
                     Degree = r.Next(1, testExam.DegreeNr + 1),
                     //SupervisorId = r.Next(1, testExam.InstructorsNr + 1),
-                    Availability = GenerateSmartAvailability(r,testExam, 0.9, 0.1, 0.5, 0.1)
+                    Availability = GenerateSmartAvailability(r,testExam, 0.9, 0.1, 0.7, 0.1)
                 };
                 var instructorSet = instructorsOfProgramms[newStudent.Programm];
                 newStudent.SupervisorId = instructorSet.ElementAt(r.Next(instructorSet.Count));
@@ -200,7 +222,8 @@ namespace FES_data_generator
             double trueToTrue = GenerateRandomDouble(Tmean - Tvar, Tmean + Tvar);
             double falseToTrue = GenerateRandomDouble(Fmean - Fvar, Fmean + Fvar);
             var chain = new MarkovChain(trueToTrue, falseToTrue);
-            return chain.GenerateStates(r.Next(2), testExam.DaysNr * testExam.SlotsPerDay / testExam.AvailabilitySlotsLenght);
+            var initState = r.NextDouble() < ((Tmean + Fmean) / 2.0) ? 1 : 0;
+            return chain.GenerateStates(initState, testExam.DaysNr * testExam.SlotsPerDay / testExam.AvailabilitySlotsLenght);
         }
 
         public static double GenerateRandomDouble(double min, double max)
